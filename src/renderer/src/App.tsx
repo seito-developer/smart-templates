3e4dd376df4bb4f5857e4d58aae12546c32ec1f0
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-02-23 11:33:02 +0900

    fix

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
+import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+import SignUp from '@/pages/SignUp'
 
 function App(): JSX.Element {
   return (
