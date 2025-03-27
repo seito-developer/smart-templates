3a29ca987d902f9c609e981f8a7772a17ba0c47e
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-03-27 21:32:23 +0900

    fix: date

--- src/renderer/src/lib/date.ts
+++ src/renderer/src/lib/date.ts
@@ -3,7 +3,7 @@ export const add365Days = (dateStr:string):string => {
     const date = new Date(`${dateStr}T00:00:00`);
   
     // 365日を足す
-    date.setDate(date.getDate() + 365);
+    date.setDate(date.getDate() + 364);
   
     // ゼロ埋めのヘルパー関数
     const pad = (num) => String(num).padStart(2, '0');
@@ -11,7 +11,7 @@ export const add365Days = (dateStr:string):string => {
     // 年・月・日を取り出して "yyyy-mm-dd" に整形
     const y = date.getFullYear();
     const m = pad(date.getMonth() + 1);
-    const d = pad(date.getDate() - 1);
+    const d = pad(date.getDate());
   
     return `${y}-${m}-${d}`;
   }
