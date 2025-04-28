d88610feb81cae1677d494fd2d1b29aa347308a1
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-04-28 16:43:24 +0900

    success: render

---
+++ web/src/App.tsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { TEMPLATES } from '../../src/renderer/src/constants/routes'
+import InitialAppointment from '../../src/renderer/src/pages/initial-appointment'
+import SiidSales from '../../src/renderer/src/pages/siid-sales'
+import SiidStart from '../../src/renderer/src/pages/siid-start'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path={HOME} element={<Login />} /> */}
+        {/* <Route path={SIGNUP} element={<SignUp />} /> */}
+        <Route path={TEMPLATES.INITIAL_APPOINTMENT} element={<InitialAppointment />} />
+        {/* <Route path={TEMPLATES.SIID_SALES} element={<SiidSales />} /> */}
+        <Route path={TEMPLATES.SIID_START} element={<SiidStart />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
