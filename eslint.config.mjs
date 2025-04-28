5e84eefaeb7fbae12428063a0eb97eb677ce70eb
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-04-28 23:12:16 +0900

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 import eslintPluginReact from 'eslint-plugin-react'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin'
+import eslintPluginImport from 'eslint-plugin-import'
 
 export default tseslint.config(
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
@@ -20,12 +22,22 @@ export default tseslint.config(
     files: ['**/*.{ts,tsx}'],
     plugins: {
       'react-hooks': eslintPluginReactHooks,
-      'react-refresh': eslintPluginReactRefresh
+      'react-refresh': eslintPluginReactRefresh,
+      'react': eslintPluginReact,
+      '@typescript-eslint': eslintPluginTypescript,
+      'import': eslintPluginImport,
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
       ...eslintPluginReactRefresh.configs.vite.rules,
-      '@typescript-eslint/no-explicit-any': 'warning'
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react/react-in-jsx-scope': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/namespace': 'error'
     }
   },
   eslintConfigPrettier
--- web/eslint.config.js
+++ web/eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin'
+import eslintPluginImport from 'eslint-plugin-import'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +18,9 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': eslintPluginReact,
+      '@typescript-eslint': eslintPluginTypescript,
+      'import': eslintPluginImport, 
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +28,14 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react/react-in-jsx-scope': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/namespace': 'error',
     },
   },
 )
