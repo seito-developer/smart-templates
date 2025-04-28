7640fa366d1d6279edd588cd479de95c4fedc07e
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-04-28 16:02:51 +0900

    add: turbo, vite,ts,react

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,15 @@
+// import { Header } from "@repo/ui/header";
+import "./style.css";
+// import typescriptLogo from "/typescript.svg";
+// import { Counter } from "@repo/ui/counter";
+// import { setupCounter } from "@repo/ui/setup-counter";
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
